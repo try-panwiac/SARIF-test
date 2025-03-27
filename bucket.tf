696d1b5632a41a3b2acd53db0438a0fd5caa97b6
Taylor <tsmith.v11@gmail.com>
2025-03-27 13:10:41 -0700

    Initial commit

---
+++ bucket.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "foo-bucket" {
+  region        = "us-east-1"
+  bucket        = "my-bucket"
+  acl           = "public-read"
+}
+
+
+resource "aws_s3_bucket_acl" "fail_2" {
+  bucket = "name"
+  access_control_policy {
+
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/global/AuthenticatedUsers"
+      }
+      permission = "READ_ACP"
+    }
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
